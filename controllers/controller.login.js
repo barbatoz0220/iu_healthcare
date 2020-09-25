c27d3a6548def054e062bd6d5ef7e80c9bab074b
TRAN TRI <ITITIU18132@student.hcmiu.edu.vn>
2020-09-25 20:52:59 +0700

    .

--- controllers/controller.login.js
+++ controllers/controller.login.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var express = require('express');
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'tf110500',
+    host: 'host',
+    user: 'user',
+    password: 'password',
     database: 'hms'
 })
 
@@ -18,6 +18,7 @@ module.exports.login = function(req, res){
 	var password = req.body.password;
 	if (username && password) {
 		connection.query('SELECT * FROM accounts a, Patient p WHERE a.pid = p.id and username = ? AND password = ?', [username, password], function(error, results, fields) {
+			console.log(results);
 			if (results.length > 0) {
 				req.session.loggedin = true;
 				req.session.username = results[0].NAME;
