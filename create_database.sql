d1ff4af9606cd0bd3693bcb01822bbadbc3bd7ff
TRAN TRI <ITITIU18132@student.hcmiu.edu.vn>
2020-09-30 23:38:26 +0700

    update query to create account in doctor

--- create_database.sql
+++ create_database.sql
@@ -6,13 +6,16 @@ create table Patient (
     DOCTOR_ID int not null,
     DISEASE_ID int not null,
     ROOM_ID int not null,
+    accid int(11),
     primary key (ID)
+   
 );
 
 create table Doctor (
 	ID int not null auto_increment,
     NAME varchar(100) not null,
-	primary key (ID)
+	accid int(11),
+    primary key (ID)
 );
 
 create table Disease (
@@ -28,6 +31,20 @@ create table Room (
     primary key (ID)
 );
 
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `Role` varchar(255) NOT NULL,
+  primary key (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+alter table Patient
+add foreign key (accid) references accounts(id);
+
+alter table Doctor
+add foreign key (accid) references accounts(id);
+
 alter table Patient
 add foreign key (DOCTOR_ID) references Doctor(ID);
 
@@ -36,6 +53,9 @@ add foreign key (DISEASE_ID) references Disease(ID);
 
 alter table Patient
 add foreign key (ROOM_ID) references Room(ID);
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `Role`) VALUES (1, 'test', 'test', 'Patient');
+INSERT INTO `accounts` (`id`, `username`, `password`, `Role`) VALUES (2, 'test2', 'test2', 'Doctor');
 
 insert into Room (NUMBER, TYPE)
 values 	(100, "ICU"), 
@@ -48,25 +68,17 @@ insert into Disease (NAME)
 values ("Fever"), ("Sore throat"), ("Headache");
 
 insert into Doctor (NAME)
-values ("David"), ("Divine");
+values ("David");
+
+insert into Doctor(NAME, accid)
+values ("Beckham", 2);
+
+insert into Patient (NAME, DOCTOR_ID, DISEASE_ID, ROOM_ID, accid)
+values 	("Paul", 1, 1, 1, 1);
 
 insert into Patient (NAME, DOCTOR_ID, DISEASE_ID, ROOM_ID)
-values 	("Paul", 1, 1, 1),
-		("Pull", 1, 2, 2),
+values  ("Pull", 1, 2, 2),
         ("Pool", 1, 3, 3),
         ("Pick", 2, 2, 4),
         ("Peli", 2, 1, 5);
 
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `PID` int not null
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`, `PID`) VALUES (1, 'test', 'test', 'test@test.com', 3);
-
-ALTER TABLE `accounts` ADD PRIMARY KEY (`id`);
-ALTER TABLE `accounts` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-alter table `accounts` add foreign key (PID) references Patient(ID);
