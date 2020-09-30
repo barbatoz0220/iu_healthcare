c9c25c769269818d37ccca4eeed2cd4221d1762e
TRAN TRI <ITITIU18132@student.hcmiu.edu.vn>
2020-09-30 23:46:30 +0700

    delete db connection and add pagination between doctor and patient

--- controllers/controller.login.js
+++ controllers/controller.login.js
@@ -1,40 +1,36 @@
-var mysql = require('mysql');
-var express = require('express');
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'tf110500',
-    database: 'hms'
-})
-connection.connect();
+const connection = require('../models/dbconnection')
 
 module.exports.index = function(req, res){
 	res.render('index');
-	res.end();
 };
 
-module.exports.login = function(req, res){
+module.exports.login = function(req, res) {
     var username = req.body.username;
 	var password = req.body.password;
-
 	if (username && password) {
-		connection.query('SELECT * FROM accounts a, Patient p WHERE a.pid = p.id and username = ? AND password = ?', [username, password], function(error, results, fields) {
-			console.log(results);
+		connection.query('SELECT * FROM accounts a, Patient p WHERE a.id = p.accid and username = ? and password = ?', [username, password], function(error, results, fields) {
 			if (results.length > 0) {
 				req.session.loggedin = true;
 				req.session.username = results[0].NAME;
 				req.session.userid = results[0].ID;
-				res.redirect('/patient/' + req.session.userid);
-				//res.send("Hello" + req.session.username);
-			} else {
-				res.send('Incorrect Username and/or Password!');
-			}		
-			res.end();
-		});
-	} else {
-		res.send('Please enter Username and Password!');
-		res.end();
-	}
-	res.cookie('fakeCookie', 115);
-};
+				
+				res.cookie('fakeCookie', 0);
+
+				res.redirect("/patient");
+				res.end();
+			}
+		})
+		connection.query('SELECT * FROM accounts a, Doctor d WHERE a.id = d.accid and username = ? and password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				req.session.loggedin = true;
+				req.session.username = results[0].NAME;
+				req.session.userid = results[0].ID;
+				
+				res.cookie('fakeCookie', 1);
+
+				res.redirect("/doctor");
+				res.end();
+			}	
+		})
+	}	
+}
