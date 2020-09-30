47712437409a437890a55c74f183e359c3dd2123
TRAN TRI <ITITIU18132@student.hcmiu.edu.vn>
2020-09-30 23:46:57 +0700

    update patient controller

--- controllers/controller.patient.js
+++ controllers/controller.patient.js
@@ -2,9 +2,18 @@ var mysql = require('mysql');
 var express = require('express');
 
 module.exports.index = function(req, res) {
-    res.render('patient');
+    res.render('patients/index', {
+        id: req.session.userid,
+        name: req.session.username
+    });
 };
 
 module.exports.menu = function(req, res) {
-    res.send('Done');
+    res.render("patients/menu.pug", {
+        id: req.session.userid
+    });
+}
+
+module.exports.information = function(req, res) {
+    res.send(req.session.username + req.session.userid);
 }
