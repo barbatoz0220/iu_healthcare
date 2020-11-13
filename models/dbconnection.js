f1990bf98bdeddfd695dd3557ee1bd64dfe75d96
MinHeehuhu <55476813+minhhuy11022000@users.noreply.github.com>
2020-11-14 00:14:57 +0700

    Make emergency UI

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -5,7 +5,7 @@ const express = require('express');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Phna0220',
+    password: '8472135huy',
     database: 'hms'
 })
 
