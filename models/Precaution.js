9f7cff8b47f4c19ce3e6962606c2f2590395687b
Tran Duc Tri <tranductrilxag@gmail.com>
2021-06-20 00:57:31 +0700

    add sql scripts

---
+++ models/Precaution.js
@@ -0,0 +1,24 @@
+const connection = require("../database/connection");
+
+class Precaution {
+  static getByDisease(disease) {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        `
+        SELECT P.NAME
+        FROM PRECAUTION p
+        INNER JOIN DISEASE_PRECAUTION DP
+        ON P.ID = DP.ID
+        INNER JOIN DISEASE D
+        ON D.ID = DP.ID
+        WHERE D.NAME LIKE '${disease.diseaseName}';`,
+        (error, precautionResults) => {
+          if (error) reject(error);
+          else resolve(precautionResults);
+        }
+      );
+    });
+  }
+}
+
+module.exports = Precaution;
